a185a0b78448b2f69dd584a274b4df3210634d6d
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-02 15:51:54 +0300

    Add keys eventlistener

--- app.js
+++ app.js
@@ -83,5 +83,42 @@ numbersEl.forEach( number => {
     tempResultEl.innerText = '';
   });
   
+  clearLastEl.addEventListener('click', () => {
+    display2El.innerText = '';
+    dis2Num= '';
+  });
+
+  window.addEventListener('keydown', (e)=>{
+    if(
+      e.key === '0' ||
+      e.key === '1' || 
+      e.key === '2' ||
+      e.key === '3' ||
+      e.key === '4' ||
+      e.key === '5' ||
+      e.key === '6' ||
+      e.key === '7' ||
+      e.key === '8' ||
+      e.key === '9' ||
+      e.key === '.' 
+    ){
+      clickButtonEl(e.key)
+      // console.log(e.key)
+    }else if(
+      e.key === '+' ||
+      e.key === '-' ||
+      e.key === '/' ||
+      e.key === '%' 
+    ){
+      clickOperation(e.key);
+    }
+    else if(e.key === '*'){
+      clickOperation('x')
+      // console.log(e.key)
+    } else if( e.key == "Enter" || e.key === '='){
+      clickEqual();
+    }
+    // console.log(e.key)
+  })
   
   
