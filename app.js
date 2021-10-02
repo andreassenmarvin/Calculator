712ca5aa99f50b5072a809ffcf745c473614f996
andreassenmarvin <marvinandreassen@gmail.com>
2021-10-02 15:49:57 +0300

    Add eventlistener for equals

--- app.js
+++ app.js
@@ -62,4 +62,16 @@ numbersEl.forEach( number => {
       result = parseFloat(result) % parseFloat(dis2Num);
     }
   }
+
+  equalEl.addEventListener('click', ()=> {
+    if (!dis2Num || !dis1Num) return;
+    haveDot = false;
+    mathOperation();
+    clearVar();
+    display2El.innerText = result;
+    tempResultEl.innerText = '';
+    dis2Num = result;
+    dis1Num = '';
+  })
+  
   
